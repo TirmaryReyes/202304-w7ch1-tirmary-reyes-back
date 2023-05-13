1368e962a89ed467932d723982a6f020a8a5cad3
Carles Pueyo <cpcastells@gmail.com>
2023-05-13 13:27:03 +0200

    Add environment variables and throw server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
+import "./loadEnvironment.js";
 import express from "express";
+import createDebug from "debug";
+import mongoose from "mongoose";
+import chalk from "chalk";
 
 const app = express();
 
-export default app;
+const debug = createDebug("robots-api:root");
+
+const port = process.env.PORT ?? 3000;
+
+const mongodbConnection = process.env.MONGODB_CONNECTION!;
+
+app.listen(port, () => {
+  debug(`Listening on http://localhost:${port}`);
+});
+
+try {
+  await mongoose.connect(mongodbConnection);
+  debug("Connected to database");
+} catch (error: unknown) {
+  debug(`Connection error: ${chalk.red((error as Error).message)}`);
+}
