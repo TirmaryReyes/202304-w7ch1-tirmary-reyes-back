cfcaa1c634f6984c04748cb12ef5f5a8c3aa626d
Carles Pueyo <cpcastells@gmail.com>
2023-05-13 19:44:24 +0200

    Handle errors in getRobots controller

--- src/controllers/robotsControllers.ts
+++ src/controllers/robotsControllers.ts
@@ -1,7 +1,15 @@
+import { type NextFunction, type Request, type Response } from "express";
 import Robot from "../database/models/Robots.js";
-import { type Request, type Response } from "express";
 
-export const getRobots = async (req: Request, res: Response) => {
-  const robots = await Robot.find().exec();
-  res.status(200).json({ robots });
+export const getRobots = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const robots = await Robot.find().exec();
+    res.status(200).json({ robots });
+  } catch (error: unknown) {
+    next(error);
+  }
 };
