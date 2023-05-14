41171cfc2d91e630cb6e443ea80f669baf55170b
Carles Pueyo <cpcastells@gmail.com>
2023-05-14 15:54:13 +0200

    Modify CORS headers

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,6 +5,11 @@ import { generalError, notFoundError } from "./middlewares/errorMiddlewares.js";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
